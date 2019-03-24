bde1eec1386ac403bdfdbecdc79c4b6f32bac793
Madhu Appojireddy <madhu.appojireddy@philips.com>
2019-03-24 10:43:56 +0530

    Initialized project

---
+++ src/common/AppContext.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const appData = {
+    name: 'Pramod'
+};
+
+
+export const AppContext = React.createContext();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './app/App';
 import * as serviceWorker from './serviceWorker';
+import { appData, AppContext } from './common/AppContext';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <AppContext.Provider value={appData}>
+        <App/>
+    </AppContext.Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
